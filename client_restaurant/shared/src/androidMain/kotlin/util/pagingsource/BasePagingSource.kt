1f1ef6dfe1241536d6b3d73ed6492c3bc37496dd
Aziza <aziza.helmy35@gmail.com>
2023-09-01 18:47:48 +0300

    add BasePagingSource

--- client_restaurant/shared/src/androidMain/kotlin/util/pagingsource/BasePagingSource.kt
+++ client_restaurant/shared/src/androidMain/kotlin/util/pagingsource/BasePagingSource.kt
@@ -1,20 +1,22 @@
 package util.pagingsource
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import domain.gateway.remote.IMealRemoteGateway
+import kotlinx.coroutines.flow.Flow
 
 
-abstract class BasePagingSource<Value : Any> (
-    protected val remoteGateway: IMealRemoteGateway
-) : PagingSource<Int, Value>() {
+abstract class BasePagingSource<Value : Any> : PagingSource<Int, Value>() {
 
-     protected abstract suspend fun fetchData(page: Int): List<Value>
+    protected abstract suspend fun fetchData(page: Int, limit: Int): List<Value>
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Value> {
         val currentPage = params.key ?: 1
+        val limit = params.loadSize
         return try {
-            val response = fetchData(currentPage)
+            val response = fetchData(currentPage, limit)
             val nextKey = (currentPage + 1).takeIf { response.lastIndex >= currentPage }
             LoadResult.Page(
                 data = response,
@@ -25,10 +27,25 @@ abstract class BasePagingSource<Value : Any> (
             LoadResult.Error(e)
         }
     }
-      override fun getRefreshKey(state: PagingState<Int, Value>): Int? {
+
+    override fun getRefreshKey(state: PagingState<Int, Value>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
                 ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
         }
+    }
+
+    private fun <I : Any, P> getAllWithParameter(
+        parameter: P,
+        sourceFactory: (P) -> PagingSource<Int, I>,
+    ): Flow<PagingData<I>> {
+        return Pager(
+            config = PagingConfig(pageSize = DEFAULT_PAGE_SIZE),
+            pagingSourceFactory = { sourceFactory(parameter) }
+        ).flow
+    }
+
+    companion object {
+        private const val DEFAULT_PAGE_SIZE = 30
     }
 }
--- client_restaurant/shared/src/androidMain/kotlin/util/pagingsource/MealsPagingSource.kt
+++ client_restaurant/shared/src/androidMain/kotlin/util/pagingsource/MealsPagingSource.kt
@@ -1,7 +1,18 @@
 package util.pagingsource
 
+import domain.entity.Meal
+import domain.gateway.remote.IMealRemoteGateway
+
 /**
  * Created by Aziza Helmy on 9/1/2023.
  */
-class MealsPagingSource {
+//todo ask for meals by cuisine id here?
+class MealsPagingSource(
+    private val remoteGateway: IMealRemoteGateway,
+    private val restaurantId: String
+) : BasePagingSource<Meal>() {
+    override suspend fun fetchData(page: Int, limit: Int): List<Meal> {
+        return remoteGateway.getAllMealsByRestaurantId(restaurantId, page, limit)
+    }
+
 }
