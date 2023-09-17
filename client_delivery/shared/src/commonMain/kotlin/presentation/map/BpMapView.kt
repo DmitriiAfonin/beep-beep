7672d789dc11df67205cd319dfc5202a83589394
youssef <yr925344git@gmail.com>
2023-09-17 14:13:25 +0300

    Delivery app | add BpMapView

---
+++ client_delivery/shared/src/commonMain/kotlin/presentation/map/BpMapView.kt
@@ -0,0 +1,11 @@
+package presentation.map
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BpMapView() {
+
+    // should have the expected here
+    // but at the same time should send the location of that map js
+
+}
